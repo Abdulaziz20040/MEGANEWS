6e77185634b8b8df674289b9d6920e9e8258ca27
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2024-12-10 12:21:23 +0500

    ds

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,12 +1,9 @@
-import React from "react";
 import FilteredCategories from "../Pages/Categores";
 import Popularposts from "../Pages/Popularposts";
-import Calendar from "./../Pages/Calendar";
 import Slider from "../Pages/Slider";
 import NewPost from "../Pages/Newposts";
 import Latesvedios from "../Pages/Latesvedios";
 import Trendyvedios from "../Pages/Trendyvedios";
-import Weather from "../Pages/Weather";
 import Topposts from "../Pages/Topposts";
 
 function Home() {
@@ -16,13 +13,11 @@ function Home() {
         <FilteredCategories />
         <Slider />
         <Popularposts />
-        {/* <Calendar /> */}
         <NewPost />
       </div>
       <Latesvedios />
       <div className="container">
         <Trendyvedios />
-        {/* <Weather /> */}
         <Topposts />
       </div>
     </>
